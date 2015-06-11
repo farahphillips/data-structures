64bb7d88b34f84a242a31949beaa635dc6de505e
fmaredia <farhana.maredia@gmail.com>
2015-06-11 11:41:21 -0500

    Finished Functional-Shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,44 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  dequeue: function() {
+
+    var result = this.storage[0];
+    delete this.storage[0];
+
+    for (var i = 0; i < this.counter; i++) {
+      this.storage[i] = this.storage[i+1];
+    }
+
+    if (this.counter > 0) {
+      this.counter--;
+    }
+
+    return result;
+  },
+  size: function () {
+    return this.counter;
+  }
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var stackMethods = {
+  push : function(value) {
+    this.storage[this.counter] = value;
+    this.counter++
+  },
+  pop : function() {
+    var result = this.storage[this.counter-1];
+    delete this.storage[this.counter-1];
+
+    if (this.counter > 0) {
+      this.counter--;
+    }
+
+    return result;
+  },
+  size: function() {
+    return this.counter;
+  }
+};
 
 
