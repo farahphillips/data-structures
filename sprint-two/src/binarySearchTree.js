eb44aec515c2b96ba2c148bb0814e074ffbb021f
fmaredia <farhana.maredia@gmail.com>
2015-06-13 16:55:18 -0500

    Completed Binary Search Tree's insert method only

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,45 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(val){
+  var newTree = Object.create(treeMethods);
+
+  newTree.value = val;
+  newTree.left = null;
+  newTree.right = null;
+
+  return newTree;
 };
 
+var treeMethods = {};
+treeMethods.insert = function (val) {
+  var childTree = BinarySearchTree(val);
+
+  var recursion = function(parent) {
+    if (val < parent.value) {
+      if (parent.left === null) {
+        parent.left = childTree;
+      } else {
+        recursion(parent.left);
+      }
+    } else {
+      if (parent.right === null) {
+        parent.right = childTree;
+      } else {
+        recursion(parent.right);
+      }
+    }
+  }
+  recursion(this);
+};
+
+treeMethods.contains = function (value) {
+
+// go through each level and see if key in object === true, return true, else return false
+
+
+};
+
+treeMethods.depthFirstLog = function (cb, value) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
